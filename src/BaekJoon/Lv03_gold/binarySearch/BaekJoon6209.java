aa52a53b0849b242d732eaa3ce61274de6213788
devpigKing <devpigking@gmail.com>
2023-08-07 23:01:07 +0900

    #feat : baekJoon6209번 코드입니다.

--- src/BaekJoon/Lv03_gold/binarySearch/BaekJoon6209.java
+++ src/BaekJoon/Lv03_gold/binarySearch/BaekJoon6209.java
@@ -1,7 +1,30 @@
 package BaekJoon.Lv03_gold.binarySearch;
 
-public class BaekJoon6209 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+public class BaekJoon6209 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long d = Long.parseLong(st.nextToken());
+        long n = Long.parseLong(st.nextToken());
+        long m = Long.parseLong(st.nextToken());
+
+        long[] dis = new long[(int) n];
+
+        for (int i = 0; i < dis.length; i++) {
+            dis[i] = Long.parseLong(st.nextToken());
+        }
+
+        Arrays.sort(dis);
+
+        
     }
 }
