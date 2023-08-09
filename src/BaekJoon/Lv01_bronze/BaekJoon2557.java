852f7445612a08912212933d077989302403b205
jwhydh <devpigking@gmail.com>
2023-08-09 09:41:38 +0900

    feat : baekJoon2557번 코드입니다.

---
+++ src/BaekJoon/Lv01_bronze/BaekJoon2557.java
@@ -0,0 +1,18 @@
+package BaekJoon.Lv01_bronze;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class BaekJoon2557 {
+    public static void main(String[] args) throws IOException {
+//        System.out.println("Hello World!");
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("Hello World!");
+        bw.flush();
+        bw.close();
+
+    }
+}
